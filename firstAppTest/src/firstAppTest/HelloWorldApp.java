0f0e1d9062f9f040b766e815939f564c07dc6f5d
Lucas Mendes <lucas.mendes556@gmail.com>
2021-01-26 13:06:13 -0300

    Hello World c/ janela

---
+++ firstAppTest/src/firstAppTest/HelloWorldApp.java
@@ -0,0 +1,15 @@
+//Teste do Hello World com exibição em pequena janela de diálogo//
+
+
+package firstAppTest;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorldApp {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Hello World!");
+
+	}
+
+}
